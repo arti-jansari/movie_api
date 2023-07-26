65bd02dfa8e007f70e734e0becc70d1220c20226
arti jansari <112994989+arti-jansari@users.noreply.github.com>
2023-07-26 22:44:07 +0530

    movie

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 // import App from './App';
-// import App from './Movieapi'
-import App from './Imagegallary'
+import App from './Movieapi'
+// import App from './Imagegallary'
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom'
 
